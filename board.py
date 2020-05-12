d9a311621ad373eb395072e263824f79eaa776b7
Charlie <itscharlieliu@gmail.com>
2020-05-11 18:03:04 -0700

    Formatted with Black

--- board.py
+++ board.py
@@ -2,13 +2,17 @@ class Board:
     def __init__(self, width: int = 10, height: int = 10):
         self._board = [[0 for x in range(width)] for y in range(height)]
 
-    def set_at(self, x: int, y: int, val: bool = True):
-        self._board[y][x] = int(val)
-
     def __str__(self):
         result = ""
         for row in self._board:
             for val in row:
                 result += str(val)
             result += "\n"
+
         return result
+
+    def set_at(self, x: int, y: int, val: bool = True):
+        self._board[y][x] = int(val)
+
+    def get_at(self, x: int, y: int) -> bool:
+        return bool(self._board[y][x])
