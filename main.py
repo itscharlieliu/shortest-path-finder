6819070a7273d4d7c82563d0f45de6555f2e9134
Charlie <itscharlieliu@gmail.com>
2020-05-15 16:35:36 -0700

    Added a listener on the keyboard

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
+from pynput import keyboard
+
 from a_star import a_star
 from board import Board
 from coord import Coord
+from terminal_utils import clear_board
+
+OPTIONS = "1) Add wall | " "2) Remove Wall | " "0) Exit"
 
 
 def main():
-    board = Board()
-    print(str(board))
-    board.set_wall(Coord(5, 5))
-    board.set_wall(Coord(4, 5))
-    board.set_wall(Coord(3, 5))
-    board.set_wall(Coord(2, 5))
-    board.set_wall(Coord(1, 5))
-    board.set_wall(Coord(0, 5))
-    board.set_wall(Coord(6, 5))
-    board.set_wall(Coord(7, 5))
-    board.set_wall(Coord(8, 5))
-    a_star(board, Coord(1, 1), Coord(5, 9))
-    print(str(board))
+
+    print(OPTIONS)
+
+    def on_press(key):
+        try:
+            print("alphanumeric key {0} pressed".format(key.char))
+        except AttributeError:
+            print("special key {0} pressed".format(key))
+
+    def on_release(key):
+        print("{0} released".format(key))
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+    # width = 10
+    # height = 10
+    #
+    # board = Board(width, height)
+    #
+    # print(str(board))
+    # a_star(board, Coord(1, 1), Coord(5, 9))
+    # clear_board(height)
+    # print(str(board))
+
+    # while True:
+    #     print(OPTIONS)
+    #     if input() == "0":
+    #         break
 
 
 if __name__ == "__main__":
